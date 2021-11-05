e2d1c8c1ca08d382fc5de1e65302973e58c5826f
Prismatic Obloquy <prismatic@obloquy.work>
2021-11-05 00:53:14 -0400

    updated license

--- unit-driver.h
+++ unit-driver.h
@@ -1,42 +1,43 @@
-/**********************************************************************\
-|                          NOTE TO DEVELOPERS                          |
-+----------------------------------------------------------------------+
-| This file orchestrates all the tests. You shouldn't really need to   |
-| edit it unless you're changing *how* tests work. If you just want to |
-| add or remove tests, see all-tests.c. To change them, look for the   |
-| relevant call to UD_SUITE and UD_TEST, then edit the functions that  |
-| those use. Don't edit this file unless you know what you're doing.   |
-+----------------------------------------------------------------------+
-| Copyright 2021 Prismatic Obloquy                                     |
-|                                                                      |
-| Redistribution and use in source and binary forms, with or without   |
-| modification, are permitted provided that the following conditions   |
-| are met:                                                             |
-|                                                                      |
-| 1. Redistributions of source code must retain the above copyright    |
-| notice, this list of conditions and the following disclaimer.        |
-|                                                                      |
-| 2. Redistributions in binary form must reproduce the above copyright |
-| notice, this list of conditions and the following disclaimer in the  |
-| documentation and/or other materials provided with the distribution. |
-|                                                                      |
-| 3. Neither the name of the copyright holder nor the names of its     |
-| contributors may be used to endorse or promote products derived from |
-| this software without specific prior written permission.             |
-|                                                                      |
-| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  |
-| "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    |
-| LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    |
-| FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       |
-| COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, |
-| INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, |
-| BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;     |
-| LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER     |
-| CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT   |
-| LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN    |
-| ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE      |
-| POSSIBILITY OF SUCH DAMAGE.                                          |
-\**********************************************************************/
+/***************************************************************************\
+|                          NOTE TO DEVELOPERS                               |
++---------------------------------------------------------------------------+
+| This is Unit Driver. It just orchestrates the tests. You shouldn't edit   |
+| it unless you're changing *how* tests work. If you just want to add or    |
+| remove tests, see all-tests.c. To change them, look for the relevant call |
+| to UD_SUITE and UD_TEST, then edit the functions that those use. Don't    |
+| edit this file unless you know what you're doing.                         |
++---------------------------------------------------------------------------+
+| Source and docs at https://github.com/prismatic-obloquy/unit-driver       |
++---------------------------------------------------------------------------+
+| Copyright 2021 Prismatic Obloquy                                          |
+|                                                                           |
+| Redistribution and use in source and binary forms, with or without        |
+| modification, are permitted provided that the following conditions are    |
+| met:                                                                      |
+|                                                                           |
+| 1. Redistributions of source code must retain the above copyright notice, |
+| this list of conditions and the following disclaimer.                     |
+|                                                                           |
+| 2. Redistributions in binary form must reproduce the above copyright      |
+| notice, this list of conditions and the following disclaimer in the       |
+| documentation and/or other materials provided with the distribution.      |
+|                                                                           |
+| 3. Neither the name of the copyright holder nor the names of its          |
+| contributors may be used to endorse or promote products derived from this |
+| software without specific prior written permission.                       |
+|                                                                           |
+| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS   |
+| IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, |
+| THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR    |
+| PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE  COPYRIGHT HOLDER OR        |
+| CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,     |
+| EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,       |
+| PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR        |
+| PROFITS; OR BUSINESS INTERRUPTION) HOWEVER  CAUSED AND ON ANY THEORY OF   |
+| LIABILITY, WHETHER IN CONTRACT, STRICT  LIABILITY, OR TORT (INCLUDING     |
+| NEGLIGENCE OR OTHERWISE) ARISING IN  ANY WAY OUT OF THE USE OF THIS       |
+| SOFTWARE, EVEN IF ADVISED OF THE  POSSIBILITY OF SUCH DAMAGE.             |
+\***************************************************************************/
 
 #ifdef __UD_PREVIOUSLY__
 #   error Cannot include UnitDriver twice.
